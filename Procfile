bbee077c1496fd6d7777ed4c19d5e34a7cb61cb3
ilyasiluyanov <isiluyanov8@gmail.com>
2021-09-08 17:42:28 +0300

    Revert to eradicate files related to deploying to heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: sh start_gunicorn.sh
--- start_gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# A script only for Heroku server
-
-cd src
-sudo apt update && sudo apt -y upgrade
-sudo apt install -y python3-dev docker docker-compose
-#execute command launching services
-docker-compose up
