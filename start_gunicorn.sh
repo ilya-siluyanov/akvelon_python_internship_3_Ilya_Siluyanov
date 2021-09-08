30bcf31d6c78528d1cb960cd16e5efbc6394cb09
ilyasiluyanov <isiluyanov8@gmail.com>
2021-09-08 17:37:20 +0300

    Procfile for heroku

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -3,8 +3,7 @@
 # A script only for Heroku server
 
 cd src
-python3 manage.py makemigrations && python3 manage.py migrate
-apt update && apt -y upgrade
-apt install -y python3-dev docker docker-compose
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y python3-dev docker docker-compose
 #execute command launching services
 docker-compose up
